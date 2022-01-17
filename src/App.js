193140e9fff905de675d6a74c1ade2350dd0c0c2
willian-pessoa <willianpessoadeabreu@gmail.com>
2022-01-17 01:52:15 -0300

    change to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from './components/home/HomeScreen';
 import BldApp from './components/bldApp/BldApp';
 import MbldApp from './components/mbldApp/MbldApp';
